2d7a50c9c32957038da86ea7fa8607d1708d8211
Anna Borja <1243488+annaborja@users.noreply.github.com>
2023-11-14 15:07:44 -0800

    chore: start creating DialogueSystem level

---
+++ Source/UnrealPortfolio/Public/Characters/UpNpcCharacter.h
@@ -0,0 +1,13 @@
+// Copyright AB. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/UpCharacter.h"
+#include "UpNpcCharacter.generated.h"
+
+UCLASS()
+class UNREALPORTFOLIO_API AUpNpcCharacter : public AUpCharacter
+{
+	GENERATED_BODY()
+};
